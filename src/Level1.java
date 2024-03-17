565de980fc48d68e5abb178200f257546a85ab85
Lera <leradziamidava@gmail.com>
2024-03-17 13:32:50 -0400

    Level 1 is created.

---
+++ src/Level1.java
@@ -0,0 +1,15 @@
+public class Level1 {
+
+    // task 1
+
+    // task 2
+
+    // task 3
+
+    public static void main(String[] args) {
+
+        System.out.println("Level 1 of the game");
+
+    }
+}
+
